baa1e7e74392413a871e9a589c3768cc5e94274b
JoaoSant7 <pedro_romano7@proton.me>
2025-12-04 09:59:26 -0300

    separado backend do frontend

--- backend/src/models/User.ts
+++
@@ -1,8 +0,0 @@
-import db from '../database/knex';
-import { User } from '../types';
-
-export const findByCpf = (cpf: string) =>
-  db<User>('users').where({ cpf }).first();
-
-export const create = (user: Omit<User, 'id' | 'created_at'>) =>
-  db<User>('users').insert(user).returning('*');
